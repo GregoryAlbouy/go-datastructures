c0551b06b92355d2e86b576b527277cd971be530
Gregory Albouy <gregoryalbouy75@gmail.com>
2020-10-06 19:06:09 +0200

    refacto: tests

--- binaryheap/binaryheap_test.go
+++ binaryheap/binaryheap_test.go
@@ -3,37 +3,9 @@ package binaryheap
 import (
 	"reflect"
 	"testing"
+
+	"github.com/gregoryalbouy/go-datastructures/testx"
 )
-
-type testStruct struct {
-	v int
-}
-
-var minCompareFunc = func(A, B interface{}) int {
-	a := A.(int)
-	b := B.(int)
-
-	if a < b {
-		return 1
-	}
-	if a > b {
-		return -1
-	}
-	return 0
-}
-
-var structCompareFunc = func(A, B interface{}) int {
-	a := A.(testStruct)
-	b := B.(testStruct)
-
-	if a.v < b.v {
-		return -1
-	}
-	if a.v > b.v {
-		return 1
-	}
-	return 0
-}
 
 func TestBHInsert(t *testing.T) {
 	h := newTestBH()
@@ -46,23 +18,46 @@ func TestBHInsert(t *testing.T) {
 }
 
 func TestBHShift(t *testing.T) {
-	h := newTestBH()
-	exp := []interface{}{3, 2, 1, 0, -1, -2, -3}
-	got := func() []interface{} {
-		n := h.Len()
-		values := []interface{}{}
-		for i := 0; i < n; i++ {
-			values = append(values, h.Shift())
-		}
-		return values
-	}()
+	testcases := []testx.Testcase{
+		{
+			Desc:     "empty",
+			Input:    New(),
+			Expected: []interface{}{},
+		}, {
+			Desc:     "signed ints",
+			Input:    newTestBH(),
+			Expected: []interface{}{3, 2, 1, 0, -1, -2, -3},
+		},
+	}
 
-	if !reflect.DeepEqual(got, exp) {
-		t.Errorf("shift: expected %v, got %v ", exp, got)
+	for _, tc := range testcases {
+		got := func() []interface{} {
+			h := tc.Input.(*binaryHeap)
+			n := h.Len()
+			values := []interface{}{}
+			for i := 0; i < n; i++ {
+				values = append(values, h.Shift())
+			}
+			return values
+		}()
+
+		testx.Check(t, tc, got)
 	}
 }
 
 func TestBHMinCompareFunc(t *testing.T) {
+	minCompareFunc := func(A, B interface{}) int {
+		a := A.(int)
+		b := B.(int)
+
+		if a < b {
+			return 1
+		}
+		if a > b {
+			return -1
+		}
+		return 0
+	}
 	h := New().SetCompareFunc(minCompareFunc).InsertMany(0, 1, -3, 2, -2, 3, -1)
 	exp := []interface{}{-3, -2, -1, 2, 1, 3, 0}
 	got := h.ToSlice()
@@ -73,6 +68,23 @@ func TestBHMinCompareFunc(t *testing.T) {
 }
 
 func TestBHStructCompareFunc(t *testing.T) {
+	type testStruct struct {
+		v int
+	}
+
+	var structCompareFunc = func(A, B interface{}) int {
+		a := A.(testStruct)
+		b := B.(testStruct)
+
+		if a.v < b.v {
+			return -1
+		}
+		if a.v > b.v {
+			return 1
+		}
+		return 0
+	}
+
 	values := []interface{}{testStruct{0}, testStruct{1}, testStruct{-3},
 		testStruct{2}, testStruct{-2}, testStruct{3}, testStruct{-1}}
 	h := New().SetCompareFunc(structCompareFunc).InsertMany(values...)
